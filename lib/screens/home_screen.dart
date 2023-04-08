39effe5914b818397c526413e8a30fae458c0fe0
kalkidan12 <kalget12@gmail.com>
2023-04-09 02:28:50 +0300

    add echange service api

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:currency_exchange/services/currency_services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,12 +12,40 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  Future curencyList = CurrencyService().getCurrencyList();
+  List<String> currencyList = [
+    "SGD",
+    "MYR",
+    "EUR",
+    "USD",
+    "AUD",
+    "JPY",
+    "CNH",
+    "HKD",
+    "CAD",
+    "INR",
+    "DKK",
+    "GBP",
+    "RUB",
+    "NZD",
+    "MXN",
+    "IDR",
+    "TWD",
+    "THB",
+    "VND"
+  ];
+  // final getCurrencyList = () async {
+  //   final currencyList = await CurrencyService().getCurrencyList();
+  // };
+
   @override
   void initState() {
+    // TODO: implement initState
     super.initState();
-    print(curencyList);
+    print(CurrencyService().exchangeCurrency());
   }
+
+  String? selectedValueFrom;
+  String? selectedValueTo;
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +55,120 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: SafeArea(
           child: Column(
-        children: [Text('data')],
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              DropdownButtonHideUnderline(
+                child: DropdownButton2(
+                  isExpanded: true,
+                  hint: Row(
+                    children: const [
+                      Icon(
+                        Icons.list,
+                        size: 16,
+                        color: Colors.yellow,
+                      ),
+                      SizedBox(
+                        width: 4,
+                      ),
+                      Expanded(
+                        child: Text(
+                          'Select Item',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.yellow,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ],
+                  ),
+                  items: currencyList
+                      .map((item) => DropdownMenuItem<String>(
+                            value: item,
+                            child: Text(
+                              item,
+                              style: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ))
+                      .toList(),
+                  value: selectedValueFrom,
+                  onChanged: (value) {
+                    setState(() {
+                      selectedValueFrom = value as String;
+                    });
+                  },
+                  buttonStyleData: ButtonStyleData(
+                    height: 50,
+                    width: 160,
+                    padding: const EdgeInsets.only(left: 14, right: 14),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(14),
+                      border: Border.all(
+                        color: Colors.black26,
+                      ),
+                      color: Colors.redAccent,
+                    ),
+                    elevation: 2,
+                  ),
+                  iconStyleData: const IconStyleData(
+                    icon: Icon(
+                      Icons.arrow_forward_ios_outlined,
+                    ),
+                    iconSize: 14,
+                    iconEnabledColor: Colors.yellow,
+                    iconDisabledColor: Colors.grey,
+                  ),
+                  dropdownStyleData: DropdownStyleData(
+                    maxHeight: 200,
+                    width: 200,
+                    padding: null,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(14),
+                      color: Colors.redAccent,
+                    ),
+                    elevation: 8,
+                    offset: const Offset(-20, 0),
+                    scrollbarTheme: ScrollbarThemeData(
+                      radius: const Radius.circular(40),
+                      thickness: MaterialStateProperty.all<double>(6),
+                      thumbVisibility: MaterialStateProperty.all<bool>(true),
+                    ),
+                  ),
+                  menuItemStyleData: const MenuItemStyleData(
+                    height: 40,
+                    padding: EdgeInsets.only(left: 14, right: 14),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              Container(
+                decoration: BoxDecoration(color: Colors.white),
+                width: 200,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(borderSide: BorderSide.none),
+                      hintText: 'Enter Amount'),
+                ),
+              )
+            ],
+          ),
+          Container(
+            child: IconButton(
+                onPressed: () {}, icon: Icon(Icons.currency_exchange)),
+          )
+        ],
       )),
     );
   }
--- lib/services/currency_services.dart
+++ lib/services/currency_services.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:currency_exchange/constants/config_constants.dart';
 import 'package:http/http.dart' as http;
 
@@ -9,7 +11,20 @@ class CurrencyService {
 
   Future<dynamic> getCurrencyList() async {
     try {
-      final url = Uri.parse(Constants.BaseUrl);
+      final url = Uri.parse(Constants.BaseUrlForCurrencyList);
+      http.Response response = await http.get(url, headers: headers);
+      if (response.statusCode == 200) {
+        print(response.body);
+        return response.body;
+      }
+    } catch (e) {}
+  }
+
+  Future<dynamic> exchangeCurrency() async {
+    Map<String, String> params = {'from': 'SGD', 'to': 'MYR', 'q': '1.0'};
+
+    try {
+      final url = Uri.https(Constants.BaseUrlForExchange, jsonEncode(params));
       http.Response response = await http.get(url, headers: headers);
       if (response.statusCode == 200) {
         print(response.body);
