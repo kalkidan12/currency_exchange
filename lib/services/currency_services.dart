9d8df0fa25de3b373dec5d9793ac102eefab88f6
kalkidan12 <kalget12@gmail.com>
2023-04-09 00:44:21 +0300

    add config for api

--- lib/services/currency_services.dart
+++ lib/services/currency_services.dart
@@ -1,1 +1,12 @@
+import 'package:currency_exchange/constants/config_constants.dart';
 
+class CurrencyService {
+  final options = {
+    'method': 'GET',
+    'url': Constants.BaseUrl,
+    'headers': {
+      'X-RapidAPI-Key': Constants.RapidAPIKey,
+      'X-RapidAPI-Host': Constants.RapidAPIHost
+    }
+  };
+}
